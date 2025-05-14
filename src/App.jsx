c69f867ad5a5cdd69a724660984078bed84b7741
ricardogodi <gdr1685@gmail.com>
2025-05-14 00:04:21 -0500

    Mobile Optimization

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import React, { useState, useRef } from "react";
 import AppController from "./lib/AppController.js";
+
 import TonicSelectionPanel from "./components/TonicSelectionPanel/TonicSelectionPanel";
 import ScaleTypeSelectionPanel from "./components/ScaleTypeSelectionPanel/ScaleTypeSelectionPanel";
-import ChordTypeSelectionPanel from "./components/ChordTypeSelectionPanel/ChordTypeSelectionPanel"
+import ChordTypeSelectionPanel from "./components/ChordTypeSelectionPanel/ChordTypeSelectionPanel";
 import ChordBoxList from "./components/ChordBoxList/ChordBoxList";
 import Bars from "./components/Bars/Bars";
 import StyleSelector from "./components/StyleSelector/StyleSelector";
@@ -11,67 +12,67 @@ import PlayerControls from "./components/PlayerControls/PlayerControls";
 import "./App.css";
 
 function App() {
+  const appController = useRef(null);
+  if (!appController.current) {
+    appController.current = new AppController();
+  }
 
-    const appController = useRef(null);
+  const [chords, setChords] = useState(appController.current.getFullChordNames());
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [chordIsDragging, setChordIsDragging] = useState(false);
+  const [numOfBars, setNumOfBars] = useState(4);
 
-    if (!appController.current) {
-        appController.current = new AppController();
-    }
-    
-    const [chords, setChords] = useState(appController.current.getFullChordNames());
-    const [isPlaying, setIsPlaying] = useState(false);
-    const [chordIsDragging, setChordIsDragging] = useState(false)
-    const [numOfBars, setNumOfBars] = useState(4)
-
-    return (
-        <div className="app">
-            <h1>Backing Track Creator</h1>
-            <div className="mainContent">
-                <div className="leftPanel">
-                    <TonicSelectionPanel
-                        controller={appController.current}
-                        setChords={setChords}
-                        isPlaying={isPlaying}
-                    />
-                    <ScaleTypeSelectionPanel
-                        controller={appController.current}
-                        setChords={setChords}
-                        isPlaying={isPlaying}
-                    />
-                    <ChordTypeSelectionPanel
-                        controller={appController.current}
-                        setChords={setChords}
-                        isPlaying={isPlaying}
-                    />
-                </div>
-                <div className="middleContent">
-                    <ChordBoxList
-                        chordList={chords}
-                        isPlaying={isPlaying}
-                        setChordIsDragging={setChordIsDragging}
-                    />
-                    <Bars
-                        controller={appController.current}
-                        isPlaying={isPlaying}
-                        numOfBars={numOfBars}
-                        setChordIsDragging={setChordIsDragging}
-                        chordIsDragging={chordIsDragging}
-                    />
-                    <StyleSelector
-                        controller={appController.current}
-                        isPlaying={isPlaying}
-                    />
-                </div>
-                <div className="rightPanel">
-                    <PlayerControls
-                        controller={appController.current}
-                        isPlaying={isPlaying}
-                        setIsPlaying={setIsPlaying}
-                    />
-                </div>
-            </div>
+  return (
+    <div className="app">
+      <h1>Backing Track Creator</h1>
+      <div className="mainContent">
+        <div className="leftPanel">
+          <TonicSelectionPanel
+            controller={appController.current}
+            setChords={setChords}
+            isPlaying={isPlaying}
+          />
+          <ScaleTypeSelectionPanel
+            controller={appController.current}
+            setChords={setChords}
+            isPlaying={isPlaying}
+          />
+          <ChordTypeSelectionPanel
+            controller={appController.current}
+            setChords={setChords}
+            isPlaying={isPlaying}
+          />
         </div>
-    );
+
+        <div className="middleContent">
+          <ChordBoxList
+            chordList={chords}
+            isPlaying={isPlaying}
+            setChordIsDragging={setChordIsDragging}
+          />
+          <Bars
+            controller={appController.current}
+            isPlaying={isPlaying}
+            numOfBars={numOfBars}
+            setChordIsDragging={setChordIsDragging}
+            chordIsDragging={chordIsDragging}
+          />
+          <StyleSelector
+            controller={appController.current}
+            isPlaying={isPlaying}
+          />
+        </div>
+
+        <div className="rightPanel">
+          <PlayerControls
+            controller={appController.current}
+            isPlaying={isPlaying}
+            setIsPlaying={setIsPlaying}
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;

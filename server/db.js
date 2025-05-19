682d1614621eab627a6df4b8ed87bd4798d8cc38
ricardogodi <gdr1685@gmail.com>
2025-05-18 19:19:30 -0500

    database connection changed

--- server/db.js
+++ server/db.js
@@ -1,9 +1,11 @@
-/** server/db.js */
 import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://127.0.0.1:27017/backing-track-db');
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log('MongoDB connected');
   } catch (err) {
     console.error('MongoDB connection failed:', err.message);
